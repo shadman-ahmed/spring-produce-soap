243e492e94c1d893ba676cbde875e939f3070b1e
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2020-03-17 10:35:17 +0600

    Initial commit

---
+++ src/main/java/com/shadmanahmed/producesoapservice/ProduceSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadmanahmed.producesoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduceSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduceSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadmanahmed/producesoapservice/ProduceSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadmanahmed.producesoapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduceSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
